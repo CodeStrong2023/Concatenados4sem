57cf75cc094d57189218be6e451472fcd5fe8b79
Franco Pieroni <71734317+franco594@users.noreply.github.com>
2024-11-08 21:53:15 -0300

    Clase 5 Node
    Ejecutamos viendo la asincronía de Node

---
+++ Node/app3.js
@@ -0,0 +1,16 @@
+// Son instrucciones no bloqueables, todo es síncrono
+console.log('Inicio del programa'); // 1
+
+setTimeout(() => {
+    console.log('Primer Timeout'); // 5
+}, 3000)
+
+setTimeout(() => {
+    console.log('Segundo Timeout'); // 3
+}, 0)
+
+setTimeout(() => {
+    console.log('Tercero Timeout'); // 4
+}, 0)
+
+console.log('Fin del programa'); // 2
