cc35df55102bd951cb7631698d4ea0304a947bdd
YvetteFrites <130080466+YvetteFrites@users.noreply.github.com>
2024-11-12 11:40:36 -0300

    Proyecto nuevo pygame - Parte 2 - Clase 8

---
+++ Python/PYGAME.PROYECT/Clase08/constantes.py
@@ -0,0 +1,14 @@
+# Dimensiones pantalla
+SCREEN_WIDTH =1700
+SCREEN_HEIGHT = 1000
+
+
+# Ruta de assets
+ASSETS_PATH = '/assets'
+
+# Ruta archivos
+IMPERIAL_MARCH_PATH = f'{ASSETS_PATH}/sounds/imperial_march.mp3'
+
+START_IMAGE_PATH = f'{ASSETS_PATH}/images/inicio/start.png'
+ESTRELLA_PATH = f'{ASSETS_PATH}/images/inicio/estrella'
+FONDO1_PATH = f'{ASSETS_PATH}/images/fondo1.jpg'
